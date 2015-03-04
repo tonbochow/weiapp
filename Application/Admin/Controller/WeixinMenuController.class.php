c7c0dc1f797d807056f747b6476ecb0f5b992d9f
tonbo <tonbochow@qq.com>
2015-03-04 22:46:46 +0800

    微信公众平台菜单功能

--- Application/Admin/Controller/WeixinMenuController.class.php
+++ Application/Admin/Controller/WeixinMenuController.class.php
@@ -112,28 +112,28 @@ class WeixinMenuController extends FoodBaseController {
 
     //编辑微信菜单(前台)
     public function edit() {
-        if(IS_POST){
+        if (IS_POST) {
             $menu_data = I('post.');
             unset($menu_data['menu_type_arr']);
-            if($menu_data['pid'] == 0){//更新二级菜单p_order
+            if ($menu_data['pid'] == 0) {//更新二级菜单p_order
                 $submenu_data['p_order'] = $menu_data['p_order'];
                 $submenu_data['update_time'] = time();
-                $submenu_save = M('WeixinMenu')->where(array('pid'=>$menu_data['id'],'member_id'=>UID,'mp_id'=>MP_ID))->save($submenu_data);
+                $submenu_save = M('WeixinMenu')->where(array('pid' => $menu_data['id'], 'member_id' => UID, 'mp_id' => MP_ID))->save($submenu_data);
             }
             $weixinMenuModel = D('WeixinMenu');
-            if($weixinMenuModel->create($menu_data)){
+            if ($weixinMenuModel->create($menu_data)) {
                 $weixin_menu_save = $weixinMenuModel->save();
-                if($weixinMenuModel){
-                    $this->success('更新微信菜单成功!','',true);
+                if ($weixinMenuModel) {
+                    $this->success('更新微信菜单成功!', '', true);
                 }
-                $this->error('更新微信菜单失败!','',true);
-            }else{
-                $this->error($weixinMenuModel->getError(),'',true);
+                $this->error('更新微信菜单失败!', '', true);
+            } else {
+                $this->error($weixinMenuModel->getError(), '', true);
             }
         }
         $id = intval(I('get.id'));
-        $weixin_menu = M('WeixinMenu')->where(array('id'=>$id,'member_id'=>UID,'mp_id'=>MP_ID))->find();
-        if($weixin_menu == false){
+        $weixin_menu = M('WeixinMenu')->where(array('id' => $id, 'member_id' => UID, 'mp_id' => MP_ID))->find();
+        if ($weixin_menu == false) {
             $this->error('您无权编辑此菜单!');
         }
         $menu_type = \Admin\Model\WeixinMenuModel::getWxCaMenuType(null, false);
@@ -141,8 +141,8 @@ class WeixinMenuController extends FoodBaseController {
             $menu_type_arr[] = array('id' => $id, 'menu_type' => $val);
         }
         $this->assign('menu_type_arr', json_encode($menu_type_arr));
-        $this->assign('weixin_menu',$weixin_menu);
-        $this->assign('json_weixin_menu',  json_encode($weixin_menu));
+        $this->assign('weixin_menu', $weixin_menu);
+        $this->assign('json_weixin_menu', json_encode($weixin_menu));
         $this->meta_title = '编辑微餐饮公众平台菜单';
         $this->display('edit');
     }
@@ -162,7 +162,7 @@ class WeixinMenuController extends FoodBaseController {
         $menu_data['status'] = \Admin\Model\WeixinMenuModel::$STATUS_ENABLE;
         $menu_data['update_time'] = time();
         $weixin_menu_enable = $weixinMenuModel->where($map)->save($menu_data);
-        if($weixin_menu_enable){
+        if ($weixin_menu_enable) {
             $this->success('启用微信公众平台菜单成功!');
         }
         $this->error('启用微信公众平台菜单失败!');
@@ -183,62 +183,81 @@ class WeixinMenuController extends FoodBaseController {
         $menu_data['status'] = \Admin\Model\WeixinMenuModel::$STATUS_DISABLE;
         $menu_data['update_time'] = time();
         $weixin_menu_enable = $weixinMenuModel->where($map)->save($menu_data);
-        if($weixin_menu_enable){
+        if ($weixin_menu_enable) {
             $this->success('禁用微信公众平台菜单成功!');
         }
         $this->error('禁用微信公众平台菜单失败!');
     }
 
     //一键生成推荐菜单(前台)
-    public function recommend(){
+    public function recommend() {
         $this->meta_title = '一键生成推荐菜单';
         $this->display('recommend');
     }
-    
+
     //一键生成自定义菜单(前台)
-    public function custom(){
+    public function custom() {
         $weixinMenuModel = M('WeixinMenu');
         $map['member_id'] = UID;
         $map['mp_id'] = MP_ID;
         $map['status'] = \Admin\Model\WeixinMenuModel::$STATUS_ENABLE;
         $weixin_menu = $weixinMenuModel->where($map)->order('p_order,c_order,create_time')->select();
-        if($weixin_menu == false){
-            $this->error('您未创建自定义菜单!');
+        if ($weixin_menu == false) {
+            if (IS_POST) {
+                $this->error('您未创建自定义菜单!', '', true);
+            } else {
+                $this->error('您未创建自定义菜单!');
+            }
         }
-//        dump($weixin_menu);
-        foreach($weixin_menu as $menu){
-            if($menu['pid'] == 0){
+        foreach ($weixin_menu as $menu) {
+            if ($menu['pid'] == 0) {
                 $top_menu_arr[$menu['id']] = $menu;
-            }else{
+            } else {
                 $sub_menu_arr[] = $menu;
             }
         }
-        
+
         $menu_arr = array();
-        foreach($top_menu_arr as $top_menu){
+        foreach ($top_menu_arr as $top_menu) {
             $top_arr = array();
-            if(!empty($sub_menu_arr)){
-                foreach($sub_menu_arr as $sub_menu){
-                    if($sub_menu['pid'] == $top_menu['id']){
+            if (!empty($sub_menu_arr)) {
+                foreach ($sub_menu_arr as $sub_menu) {
+                    if ($sub_menu['pid'] == $top_menu['id']) {
                         $top_arr[] = $sub_menu;
                     }
                 }
-                if(empty($top_arr)){
-                    $menu_arr[$top_menu['id']]= '';
-                }else{
+                if (empty($top_arr)) {
+                    $menu_arr[$top_menu['id']] = '';
+                } else {
                     $menu_arr[$top_menu['id']] = $top_arr;
                 }
-            }else{
+            } else {
                 $menu_arr[$top_menu['id']][] = $top_menu;
             }
         }
-//        dump($menu_arr);exit;
-//        dump($top_menu_arr);
-//        dump($sub_menu_arr);
-        $this->assign('menu_arr',$menu_arr);
-        $this->assign('top_menu_arr',$top_menu);
+        if (IS_POST) {//生成微信公众平台菜单
+            $custom = I('post.custom');
+            if ($custom) {
+                $wx_menu_str = '{"button":[';
+                foreach ($menu_arr as $pid => $sub_arr) {
+                    if (!empty($sub_arr)) {
+                        echo "<td valign='bottom' align='center'><table>";
+                        foreach ($sub_arr as $sub) {
+                            echo "<tr><td valign='bottom' align='center' style='background-color:#fff;padding:1px;'>" . '<a class="btn" href="/Admin/WeixinMenu/edit/id/' . $sub['id'] . '">' . $sub['menu_name'] . "[二级]</a></td></tr>";
+                        }
+                        echo "<tr><td valign='bottom' align='center' style='background-color:#fff;padding:1px;'>" . '<a class="btn submit-btn" href="/Admin/WeixinMenu/edit/id/' . $sub['id'] . '">' . $top_menu_arr[$pid]['menu_name'] . "[一级菜单]</a></td></tr>";
+                        echo "</table></td>";
+                    } else {
+                        echo "<td valign='bottom' align='center' style='background-color:#fff;padding:1px;'>" . '<a class="btn submit-btn" href="/Admin/WeixinMenu/edit/id/' . $top_menu_arr[$pid]['id'] . '">' . $top_menu_arr[$pid]['menu_name'] . "[一级菜单]</a></td>";
+                    }
+                }
+                $wx_menu_str .= "]}";
+            }
+        }
+        $this->assign('menu_arr', $menu_arr);
+        $this->assign('top_menu_arr', $top_menu_arr);
         $this->meta_title = '一键生成自定义菜单';
         $this->display('custom');
     }
-    
+
 }
