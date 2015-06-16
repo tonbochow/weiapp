e50afd879d122712a6355839ddd93bf7a7b90aa5
tonbo <tonbochow@qq.com>
2015-06-16 10:00:24 +0800

    卡劵测试白名单修改

--- Application/Wechat/Controller/WhiteListController.class.php
+++ Application/Wechat/Controller/WhiteListController.class.php
@@ -15,7 +15,7 @@ class WhiteListController extends Controller {
 
     public function index() {
         $list = '{
-                  "username":["chowtonbo"]
+                  "username":["chowtonbo","qunxing1234"]
                  }';
         $add_res = \Admin\Model\WxCardModel::testWhiteList('wx571d493fc32f0ba3', '6677a350c853729910c9481dab475570', $list);
         dump($add_res);
